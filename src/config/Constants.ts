80cc87f2d5813b30ca587ff3d3f602d74a210e44
rwp10@hotmail.com <rwp10@hotmail.com>
2021-11-22 13:33:58 -0300

    Projeto inicial

---
+++ src/config/Constants.ts
@@ -0,0 +1,6 @@
+export const config = {
+	API_URL: 'http://localhost:5000',
+	MONGO_CONNECTION:
+//		'mongodb+srv://<user>:<password>@<database>.yptlq.mongodb.net/url-shortener-dio?retryWrites=true&w=majority',
+	 'mongodb+srv://ronaldo:%40Wcd7531590@cluster0.1vath.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+}
